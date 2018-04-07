fb09b0961fbc7a5ab58aeab3187804cb5871cdb3
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-07 23:45:03 +0000

    automatically backed up by learn

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe PostController, type: :controller do
+RSpec.describe PostsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
